b83d075b9bc3b6877a2f5ac888e824c26bb94bcd
morispolanco <morispolanco@gmail.com>
2023-12-17 23:09:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,27 @@
 import streamlit as st
 import cohere
 
-# Inicializar el cliente de Cohere
-cohere_api_key = 'KDhsUSaH5D01hlEWHEOfZvNhowIGPZC2cUcCP1sO'  # Reemplaza con tu clave API
-co = cohere.Client(cohere_api_key)
+co = cohere.Client('KDhsUSaH5D01hlEWHEOfZvNhowIGPZC2cUcCP1sO')
 
-# Aplicación Streamlit
-def main():
-    st.title("Preguntas y respuestas sobre las leyes de Guatemala")
+st.title("Leyes de Guatemala")
 
-    # Entrada del usuario
-    pregunta_usuario = st.text_input("Haz una pregunta sobre las leyes de Guatemala:")
+question = st.text_input("Escribe tu pregunta:")
 
-    if st.button("Obtener respuesta"):
-        # Llamar a la API de Cohere
-        respuesta = co.chat(
-            model='command',
-            message=pregunta_usuario,
-            temperature=0.3,
-            chat_history=[{"role": "User", "message": pregunta_usuario}],
-            prompt_truncation='AUTO',
-            stream=True,
-            citation_quality='accurate',
-            connectors=[{"id": "web-search"}],
-            documents=[]
-        )
+if st.button("Responder"):
 
-        # Convertir la respuesta a cadena antes de mostrarla
-        st.text("Respuesta completa de Cohere:")
-        st.write(str(respuesta))
+  response = co.chat(
+    model='command',
+    message=question,
+    temperature=0.3,
+    chat_history=[
+      {"role": "User", "message": question}
+    ],
+    prompt_truncation='AUTO',
+    stream=True,
+    citation_quality='accurate',
+    connectors=[{"id":"web-search"}], 
+    documents=[]
+  )
 
-        # Mostrar las respuestas sugeridas al usuario
-        if "suggestedUserResponses" in respuesta:
-            st.text("Respuestas sugeridas:")
-            st.write(respuesta["suggestedUserResponses"])
-
-if __name__ == "__main__":
-    main()
+  st.write("Respuesta:")
+  st.write(response)
